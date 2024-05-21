165363a6dc0c75b9630dee4190df6929ec3ff647
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2024-05-22 00:15:02 +0400

    first commit

--- AdiniBilmediyim/Playlist/Program.cs
+++ AdiniBilmediyim/Playlist/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace Playlist
+{
+    public enum MusicGenre
+    {
+        Pop,
+        Rock,
+        Jazz,
+        Classical
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Song song1 = new Song("Shape of you", "Ed Sheeran", new TimeSpan(00, 03, 00), Genre.Rock);
+            Playlist playlist = new Playlist();
+            playlist.AddSong(song1);
+            playlist.GetAll();
+            playlist.GetAllSongsByGenre(MusicGenre.Rock);
+        }
+    }
+}
